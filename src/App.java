94788343a7faf1edcd3a85ad309511455a001891
modengann <99417989+modengann@users.noreply.github.com>
2022-11-01 10:34:57 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,43 @@
+/*** Instructions:
+ *   Complete the method twoLetters based on the requirements listed below
+ * 
+ *   The method twoLetters will accept two variables as parameters: a String and an int called index. The method header has been completed already
+ *   Based on these parameters the method will return a String that is two characters long starting from the given index.
+ * 
+ *   For example:
+ *   twoLetters("java", 0) will return "ja" because the given index was 0, so it returns the first two letters of the given word
+ *   
+ *   For example:
+ *   twoLetters("java", 2) will return "va" because in this case we are starting at index location 2 (letter 'v') and 
+ *   returning a String made out of that and the next letter
+ * 
+ *   For example:
+ *   twoLetters("java", 3) will return "ja" because unlike in other cases, when we start at index 3 we will be at the end of the provided String.
+ *   So we don't have remaining letters to return a String with length 2. Therefore, based on the rules listed above we return the first two letters "ja".
+ *  
+ *   *   A+ bonus challenge: 
+*   Add additional features to the code to demonstrate mastery. Be creative! Show off what you know!
+*/
+
+
+public class App {
+    
+    //All your work should be in the method twoLetters(). The main method will just be for testing. Sample tests are commented out
+    //Remove the comments in the main method to run a few tests on your code, or make your own tests.
+    public static void main(String[] args) {
+
+        //Below you can find some samples of how this method should work. Uncomment to test your code
+        //System.out.println("With an input of java and 0, the output should be: ja. Your output: " + twoLetters("java", 0));
+        //System.out.println("With an input of java and 2, the output should be: va. Your output: " + twoLetters("java", 2));
+        //System.out.println("With an input of java and 3, the output should be: ja. Your output: " + twoLetters("java", 3));
+
+    }
+
+    public static String twoLetters(String word, int index){
+        //complete your code here
+        return "";
+    }
+
+    
+
+}
